c30b2af358afff98b1bbd8282415171f5815cac1
Carlos Ventas Lara <carlosventas_1996@hotmail.com>
2024-02-10 17:56:57 +0100

    Cambios rama1: introduccion getter edad

--- PruebaGit_rev1/src/main/Persona.java
+++ PruebaGit_rev1/src/main/Persona.java
@@ -14,6 +14,7 @@ public class Persona {
 	//Getter
 	public String getNombre() {return this.nombre;}
 	public String getApellidos() {return this.apellidos;}
+	public int getEdad() {return this.edad;}
 
 	
 	//Setter
